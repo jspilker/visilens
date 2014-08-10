4a3eaf5a64a29e928a1c74ab96f27110b9de4a1d
Justin Spilker <spilkerj@gmail.com>
2014-08-10 16:04:21 -0700

    added sersic profile

--- Model_objs.py
+++ Model_objs.py
@@ -50,15 +50,23 @@ class SersicSource(object):
       """
       Create a symmetric Sersic profile object, with 
       I(x,y) = A * exp(-(r/alpha)^(1/n)),
-      where n is the Sersic index (0.5=gaussian,1=disk,4=elliptical),
-      alpha is the scale radius, and r=sqrt(x**2+y**2).
+      where n is the Sersic index (0.5=gaussian,1=expdisk,4=elliptical),
+      alpha is the scale radius, and r=sqrt(x**2+y**2). To make a non-
+      axisymmetric profile, we rescale x and y above to elliptical coords.
 
-      Specified here by the integrated flux (NOT peak amplitude).
+      Specified here by the integrated flux (NOT peak amplitude). This helps
+      avoid problems for large indices (even n~4), where tons of flux are
+      spread to large distances.
       
-      Units of parameters are xoff,yoff: arcsec rel to lens; flux: Jy; width: alpha in arcsec
+      If lensed is True, it's assumed this source is in the background, and we
+      lens it; otherwise it's assumed to be a foreground/unlensed object.
+      
+      Units of parameters are xoff,yoff: arcsec rel to lens; flux: Jy; 
+            width: alpha in arcsec; axisratio: minor/major; PA: deg CCW from x-axis
       """
 
-      def __init__(self,z,xoff,yoff,flux,alpha,index):
+      def __init__(self,z,lensed=True,xoff=None,yoff=None,flux=None,alpha=None,\
+                  index=None,axisratio=None,PA=None):
             # Do some input handling.
             if not isinstance(xoff,dict):
                   xoff = {'value':xoff,'fixed':False,'prior':[-10.,10.]}
@@ -70,8 +78,12 @@ class SersicSource(object):
                   alpha = {'value':alpha,'fixed':False,'prior':[0.,2.]} # arcsec
             if not isinstance(index,dict):
                   index = {'value':index,'fixed':False,'prior':[1/3.,10]}
+            if not isinstance(axisratio,dict):
+                  axisratio = {'value':axisratio,'fixed':False,'prior':[0.01,1.]}
+            if not isinstance(PA,dict):
+                  PA = {'value':PA,'fixed':False,'prior':[0.,180.]}
 
-            if not all(['value' in d for d in [xoff,yoff,flux,alpha,index]]): 
+            if not all(['value' in d for d in [xoff,yoff,flux,alpha,index,axisratio,PA]]): 
                   raise KeyError("All parameter dicts must contain the key 'value'.")
 
             if not 'fixed' in xoff: xoff['fixed'] = False
@@ -79,30 +91,40 @@ class SersicSource(object):
             if not 'fixed' in flux: flux['fixed'] = False  
             if not 'fixed' in alpha: alpha['fixed'] = False
             if not 'fixed' in index: index['fixed'] = False
+            if not 'fixed' in axisratio: axisratio['fixed'] = False
+            if not 'fixed' in PA: PA['fixed'] = False
             
             if not 'prior' in xoff: xoff['prior'] = [-10.,10.]
             if not 'prior' in yoff: yoff['prior'] = [-10.,10.]
             if not 'prior' in flux: flux['prior'] = [1e-5,1.]
             if not 'prior' in alpha: alpha['prior'] = [0.,2.]
             if not 'prior' in index: index['prior'] = [1/3.,10]
+            if not 'prior' in axisratio: axisratio['prior'] = [0.01,1.]
+            if not 'prior' in PA: PA['prior'] = [0.,180.]
 
             self.z = z
+            self.lensed = lensed
             self.xoff = xoff
             self.yoff = yoff
             self.flux = flux
             self.alpha = alpha
             self.index = index
+            self.axisratio = axisratio
+            self.PA = PA
 
 
 class GaussSource(object):
       """
       Convenience wrapper to create a symmetric Gaussian source profile, a special
       case of a Sersic profile with n=0.5 and alpha=sqrt(2)*sigma, axis ratio = 1., PA=0.
+      
+      If lensed is True, it's assumed this source is in the background, and we
+      lens it; otherwise it's assumed to be a foreground/unlensed object.
 
       Units of parameters are xoff,yoff: arcsec rel to lens; flux: Jy; width: Gaussian rms in arcsec
       """
 
-      def __init__(self,z,xoff,yoff,flux,width):
+      def __init__(self,z,lensed=True,xoff=None,yoff=None,flux=None,width=None):
             # Do some input handling.
             if not isinstance(xoff,dict):
                   xoff = {'value':xoff,'fixed':False,'prior':[-10.,10.]}
@@ -127,6 +149,7 @@ class GaussSource(object):
             if not 'prior' in width: width['prior'] = [0.,2.]
 
             self.z = z
+            self.lensed = lensed
             self.xoff = xoff
             self.yoff = yoff
             self.flux = flux
