bffce13206b74466dc49d0d98973d7eae6011d6c
Justin Spilker <spilkerj@gmail.com>
2014-08-17 14:58:55 -0700

    altered sigma levels

--- plot_images.py
+++ plot_images.py
@@ -2,13 +2,15 @@ import numpy as np
 from astropy.stats import sigma_clip
 import matplotlib.pyplot as pl
 import matplotlib.cm as cm
+from Model_objs import *
+from Data_objs import *
 from uvimage import uvimageslow
 from calc_likelihood import create_modelimage,fft_interpolate
 from modelcal import model_cal
 from GenerateLensingGrid import GenerateLensingGrid
 
-def plot_images(data,chains,xmax=30.,highresbox=[-3.,3.,-3.,3.],emitres=None,fieldres=None,
-                  imsize=512,pixsize=0.2,taper=0.):
+def plot_images(data,mcmcresult,
+                  imsize=512,pixsize=0.2,taper=0.,**kwargs):
       """
       Create a four-panel figure from data and chains,
       showing data, best-fit model, residuals, high-res image.
@@ -28,16 +30,16 @@ def plot_images(data,chains,xmax=30.,highresbox=[-3.,3.,-3.,3.],emitres=None,fie
       limits = kwargs.pop('limits',
             [-imsize*pixsize/2.,+imsize*pixsize/2.,imsize*pixsize/2.,-imsize*pixsize/2.])
       cmap = kwargs.pop('cmap',cm.Greys)
-      mapcontours = kwargs.pop('mapcontours',np.arange(-20,20,2))
-      rescontours = kwargs.pop('rescontours',np.arange(-5,5))
+      mapcontours = kwargs.pop('mapcontours',np.delete(np.arange(-21,22,3),7))
+      rescontours = kwargs.pop('rescontours',np.delete(np.arange(-5,5),5))
 
       datasets = list(np.array([data]).flatten())
 
       # shorthand for later
-      c = chains['chains']
+      c = mcmcresult['chains']
 
       # Set up to create the model image. We'll assume the best-fit values are all the medians.
-      lens,source = chains['lens_p0'], chains['source_p0']
+      lens,source = mcmcresult['lens_p0'], mcmcresult['source_p0']
       if isinstance(lens,SIELens):
             for key in ['x','y','M','e','PA']:
                   if not vars(lens)[key]['fixed']:
@@ -66,15 +68,15 @@ def plot_images(data,chains,xmax=30.,highresbox=[-3.,3.,-3.,3.],emitres=None,fie
       if 'scaleamp' in mcmcresult.keys():
             for i,t in enumerate(mcmcresult['scaleamp']): # only matters if >1 datasets
                   if i==0: pass
-                  elif t: scaleamp[i] = np.median(c['ampscale_dset'+str(i+1)])
+                  elif t: scaleamp[i] = np.median(c['ampscale_dset'+str(i)])
                   else: pass
       shiftphase = np.zeros((len(datasets),2))
       if 'shiftphase' in mcmcresult.keys():
             for i,t in enumerate(mcmcresult['shiftphase']):
                   if i==0: pass # only matters if >1 datasets
                   elif t:
-                        shiftphase[i][0] = np.median(c['astromshift_x_dset'+str(i+1)])
-                        shiftphase[i][1] = np.median(c['astromshift_y_dset'+str(i+1)])
+                        shiftphase[i][0] = np.median(c['astromshift_x_dset'+str(i)])
+                        shiftphase[i][1] = np.median(c['astromshift_y_dset'+str(i)])
                   else: pass # no shifting
 
       sourcedatamap = mcmcresult['sourcedatamap'] if 'sourcedatamap' in mcmcresult.keys() else None
@@ -84,9 +86,8 @@ def plot_images(data,chains,xmax=30.,highresbox=[-3.,3.,-3.,3.],emitres=None,fie
       
       for i,dset in enumerate(datasets):
             # Get us some coordinates.
-            if isinstance(dset,Visdata):
-                  xmap,ymap,xemit,yemit,ix = GenerateLensingGrid(datasets,mcmcresult['xmax'],\
-                        mcmcresult['highresbox'],mcmcresult['fieldres'],mcmcresult['emitres'])
+            xmap,ymap,xemit,yemit,ix = GenerateLensingGrid(datasets,mcmcresult['xmax'],\
+                  mcmcresult['highresbox'],mcmcresult['fieldres'],mcmcresult['emitres'])
             
             # Create model image
             immap,_ = create_modelimage(lens,source,shear,xmap,ymap,xemit,yemit,\
@@ -107,20 +108,21 @@ def plot_images(data,chains,xmax=30.,highresbox=[-3.,3.,-3.,3.],emitres=None,fie
             
             # Plot everything up
             ext = [-imsize*pixsize/2.,imsize*pixsize/2.,imsize*pixsize/2.,-imsize*pixsize/2.]
-            s = (sigma_clip(imsize,sig=3.)).std() # Map noise, roughly.
+            #s = (sigma_clip(imdata.flatten(),sig=3.)).std() # Map noise, roughly.
+            s = imdiff.std() # Map noise, roughly.
             axarr[i,0].imshow(imdata,interpolation='nearest',extent=ext,cmap=cmap)
             axarr[i,0].contour(imdata,extent=ext,colors='k',origin='image',levels=s*mapcontours)
             axarr[i,0].set_xlim(limits[0],limits[1]); axarr[i,0].set_ylim(limits[2],limits[3])
             axarr[i,1].imshow(immodel,interpolation='nearest',extent=ext,cmap=cmap,\
-                  vmin=imdata.min(),imdata.max())
+                  vmin=imdata.min(),vmax=imdata.max())
             axarr[i,1].contour(immodel,extent=ext,colors='k',origin='image',levels=s*mapcontours)
             axarr[i,1].set_xlim(limits[0],limits[1]); axarr[i,1].set_ylim(limits[2],limits[3])
             axarr[i,2].imshow(imdiff,interpolation='nearest',extent=ext,cmap=cmap,\
-                  vmin=imdata.min(),imdata.max())
+                  vmin=imdata.min(),vmax=imdata.max())
             axarr[i,2].contour(imdiff,extent=ext,colors='k',origin='image',levels=s*rescontours)
             axarr[i,2].set_xlim(limits[0],limits[1]); axarr[i,2].set_ylim(limits[2],limits[3])
             axarr[i,3].imshow(immap,interpolation='nearest',extent=ext,cmap=cmap)
-            axarr[i,3].set_xlim(limits[0],limits[1]); axarr[i,3].set_ylim(limits[2],limits[3])
+            axarr[i,3].set_xlim(limits[0]/3.,limits[1]/3.); axarr[i,3].set_ylim(limits[2]/3.,limits[3]/3.)
       
 
       return f,axarr
